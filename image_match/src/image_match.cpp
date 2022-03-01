6c93dd5644ff58608134edafeb925f1b9f61b5bf
LZBUAV <1245039130@qq.com>
2022-03-01 09:45:59 +0800

    cherry- pick

--- image_match/src/image_match.cpp
+++ image_match/src/image_match.cpp
@@ -4,78 +4,102 @@ namespace match{
 
 IMAGE_MATCH::IMAGE_MATCH(std::string base_feature_path)
 {
-    std::string image_path;
-    for(auto file_path : std::experimental::filesystem::directory_iterator(base_feature_path))
+    // std::string image_path;
+    // int i = 0;
+    // for(auto file_path : std::experimental::filesystem::directory_iterator(base_feature_path))
+    // {
+    //     image_path = file_path.path().string();
+    //     // std::cout << image_path << std::endl;
+    //     cv::Mat frame = cv::imread(image_path);
+    //     cv::Rect roi(0, 0, frame.cols, frame.rows);
+    //     std::vector<cv::Mat> base_feature;
+    //     // std::cout << frame.cols << " " << frame.rows << " " << roi.size() << std::endl;
+    //     get_color(frame, roi, base_feature);
+    //     base_features.push_back(base_feature);
+    //     // std::cout << i++ << std::endl;
+    // }
+    base_images_list = readFolder(base_feature_path);
+    // std::sort(base_images_list.begin(), base_images_list.end(), cmp);
+    for(auto image_name : base_images_list)
     {
-        image_path = file_path.path().string();
-        // std::cout << image_path << std::endl;
-        cv::Mat frame = cv::imread(image_path);
+        cv::Mat frame = cv::imread(image_name);
+        // std::cout << frame.size() << " " << frame.empty() << " " << image_name << std::endl;
+        // assert(!frame.empty());
         cv::Rect roi(0, 0, frame.cols, frame.rows);
         std::vector<cv::Mat> base_feature;
-        // std::cout << frame.cols << " " << frame.rows << " " << roi.size() << std::endl;
         get_color(frame, roi, base_feature);
         base_features.push_back(base_feature);
     }
+    max_images = base_images_list.size();
+}
 
+void IMAGE_MATCH::update_base_features(int index)
+{
+    cv::Mat frame = cv::imread(base_images_list[index]);
+    std::vector<cv::Mat> base_feature;
+    cv::Rect roi(0, 0, frame.cols, frame.rows);
+    get_color(frame, roi, base_feature);
+    base_features[index] = base_feature;
+    std::cout << "update the base image: " << base_images_list[index] << " index = " << index << std::endl;
 }
 
 void IMAGE_MATCH::get_color(const cv::Mat& rgbimage, const cv::Rect& roi , std::vector<cv::Mat>& cn_feature)
 {
     cv::Mat frame = rgbimage(roi);
-    cv::Mat frame_gray;
-    cv::Mat frame_gray_resized;
+    cv::Mat frame_resized;
 
-    cv::cvtColor(frame, frame_gray, CV_BGR2BGRA);
+    int w = frame.cols, h = frame.rows;
+    int size_col = w%2;
+    int size_row = h%2;
 
-    int w = frame_gray.cols, h = frame_gray.rows;
-    int size_col = w%3;
-    int size_row = h%3;
-
-    if(size_col)
+    if(size_col != 0)
     {
         w -= size_col;
     }
 
-    if(size_row)
+    if(size_row != 0)
     {
         h -= size_row;
     }
 
-    cv::resize(frame_gray, frame_gray_resized, cv::Size(w, h));
+    cv::resize(frame, frame_resized, cv::Size(w, h));
 
-    int step_col = w/3;
-    int step_row = h/3;
+    int step_col = w/2;
+    int step_row = h/2;
 
     int histsize[1] = {256};
-    int channels[1] = {0};
+    int channel_r[1] = {0};
+    int channel_g[1] = {1};
+    int channel_b[1] = {2};
     float hr1[2] = {0, 256};
     const float* ranges[] = {hr1};
 
-    for(int x = 0; x < 3; x++)
+    for(int x = 0; x < 2; x++)
     {
-        for(int y = 0; y < 3; y++)
+        for(int y = 0; y < 2; y++)
         {
-            cv::Mat hist;
+            cv::Mat hist_r, hist_g, hist_b;
             cv::Rect sub_roi(x*step_col, y*step_row, step_col, step_row);
-            cv::Mat sub_image = frame_gray_resized(sub_roi);
-            cv::calcHist(&sub_image, 1, channels, cv::Mat(), hist, 1, histsize, ranges);
+            cv::Mat sub_image = frame_resized(sub_roi);
+            cv::calcHist(&sub_image, 1, channel_r, cv::Mat(), hist_r, 1, histsize, ranges);
+            cv::calcHist(&sub_image, 1, channel_g, cv::Mat(), hist_g, 1, histsize, ranges);
+            cv::calcHist(&sub_image, 1, channel_b, cv::Mat(), hist_b, 1, histsize, ranges);
 
-            hist.convertTo(hist, CV_32FC1);
-            float sum = 0.0;
-            for(int j = 0; j < hist.rows; j++)
-            {
-                sum +=hist.at<float>(cv::Point(j, 0));
-            }
-            
-            for(int j = 0; j < hist.rows; j++)
-            {
-                hist.at<float>(cv::Point(j, 0)) /= sum + 0.000001;
-            }
-
-            cn_feature.push_back(hist);
+            cn_feature.push_back(hist_r);
+            cn_feature.push_back(hist_g);
+            cn_feature.push_back(hist_b);
 
         }
     }
+
+    cv::Mat hist_r_global, hist_g_global, hist_b_global;
+    cv::calcHist(&frame, 1, channel_r, cv::Mat(), hist_r_global, 1, histsize, ranges);
+    cv::calcHist(&frame, 1, channel_g, cv::Mat(), hist_g_global, 1, histsize, ranges);
+    cv::calcHist(&frame, 1, channel_b, cv::Mat(), hist_b_global, 1, histsize, ranges);
+
+    cn_feature.push_back(hist_r_global);
+    cn_feature.push_back(hist_g_global);
+    cn_feature.push_back(hist_b_global);
 
 }
 
@@ -85,26 +109,25 @@ double IMAGE_MATCH::compare_opencv(const cv::Mat& rgbimage, const cv::Rect& roi)
     get_color(rgbimage, roi, feature);
     
     double conficient = 0.0;
-
+    
     for(auto base_feature : base_features)
     {
         double temp_conficient = 0.0;
         for(int i = 0; i < feature.size(); i++)
         {
-            temp_conficient += cv::compareHist(feature[i], base_feature[i], cv::HISTCMP_CORREL);
+            temp_conficient += (cv::compareHist(feature[i], base_feature[i], cv::HISTCMP_CORREL)*weights[i]);
         }
-        temp_conficient /= 9.0;
         conficient = conficient > temp_conficient ? conficient : temp_conficient;
     }
 
-    std::cout << " 最大相似度 " << conficient << std::endl;
+    // std::cout << " 最大相似度 " << conficient << std::endl;
 
     return conficient;
 
 }
 
 
-bool IMAGE_MATCH::match(const darknet_ros_msgs::BoundingBoxes& bbox, cv::Mat& rgbimage, image_match::match_result& result)
+bool IMAGE_MATCH::match(darknet_ros_msgs::BoundingBoxes bbox, cv::Mat& rgbimage, image_match::match_result& result)
 {
     cv::Rect roi;
 
@@ -115,10 +138,15 @@ bool IMAGE_MATCH::match(const darknet_ros_msgs::BoundingBoxes& bbox, cv::Mat& rg
 
     for(int i = 0; i < num_boxs; i++)
     {
+        int x = bbox.bounding_boxes[i].xmin;
+        int y = bbox.bounding_boxes[i].ymin;
+        int w = bbox.bounding_boxes[i].xmax-bbox.bounding_boxes[i].xmin;
+        int h = bbox.bounding_boxes[i].ymax-bbox.bounding_boxes[i].ymin;
+
         double conficient = 0.0;
-        if(bbox.bounding_boxes[i].id == 0 && bbox.bounding_boxes[i].probability >= 0.5)
+        if(bbox.bounding_boxes[i].id == 0 && bbox.bounding_boxes[i].probability >= det_probability_thrd)
         {
-            roi = cv::Rect(bbox.bounding_boxes[i].xmin, bbox.bounding_boxes[i].ymin, bbox.bounding_boxes[i].xmax-bbox.bounding_boxes[i].xmin, bbox.bounding_boxes[i].ymax-bbox.bounding_boxes[i].ymin);
+            roi = cv::Rect(x, y, w, h); 
             conficient = compare_opencv(rgbimage, roi);
             if(conficient > max_conficient)
             {
@@ -127,28 +155,51 @@ bool IMAGE_MATCH::match(const darknet_ros_msgs::BoundingBoxes& bbox, cv::Mat& rg
             }
         }
     }
-
+    
     result.x = bbox.bounding_boxes[max_conficient_index].xmin;
     result.y = bbox.bounding_boxes[max_conficient_index].ymin;
     result.width = bbox.bounding_boxes[max_conficient_index].xmax-bbox.bounding_boxes[max_conficient_index].xmin;
     result.height = bbox.bounding_boxes[max_conficient_index].ymax-bbox.bounding_boxes[max_conficient_index].ymin;
+    result.conf = max_conficient;
+    result.is_person = 0;
 
     if(bbox.bounding_boxes[max_conficient_index].id == 0)
     {   
-        cv::rectangle(rgbimage, cv::Rect(result.x, result.y, result.width, result.height), cv::Scalar(0, 255, 255), 1, 0);
-        cv::putText(rgbimage, "max_conficient: " + std::to_string(max_conficient), cv::Point(50, 150), CV_FONT_NORMAL, 1, cv::Scalar(0, 255, 255), 3, 8);
+        result.is_person = 1;
         cv::Rect max_roi(result.x, result.y, result.width, result.height);
-        // cv::imwrite("/home/lzb/personal/project/cpptest/images/lzb/" + std::to_string(std::rand()%1000) + ".jpg", rgbimage(max_roi));
-    }
 
-    if(max_conficient >= 0.5)
-    {
-        return true;
+        //comment the if code when sampling images.
+        // if(max_conficient >= update_conficient_thrd)
+        // {
+        //     update_count++;
+        //     if(update_count == 20)
+        //     {
+        //         cv::imwrite(base_images_list[update_index], rgbimage(max_roi));
+            
+        //         update_base_features(update_index);
+                
+        //         update_index++;
+        //         if(update_index == max_images)
+        //         {
+        //             update_index = 0;
+        //         }
+        //         update_count = 0;
+        //     }
+            
+        // }
+
+        //commend the imwrite code when sample has complted.
+        // cv::imwrite("/home/nvidia/project/catkin_ws/src/image_match/images/qianmuhu/" + std::to_string(save_index) + ".jpg", rgbimage(max_roi));
+        // save_index++;
+
+        // cv::rectangle(rgbimage, cv::Rect(result.x, result.y, result.width, result.height), cv::Scalar(0, 255, 255), 1, 0);
+        // cv::putText(rgbimage, "max_conficient: " + std::to_string(max_conficient), cv::Point(result.x, result.y-5), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 255), 2);
+        std::cout << "最大相似度: " << max_conficient << std::endl;
+        
     }
-    else
-    {
-        return false;
-    }
+    
+    return true;
+    
 }
 
 }
